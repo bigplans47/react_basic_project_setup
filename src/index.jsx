d398d7d8afd8a7e6fab097fad3e7d50e0bf3b0f0
Dan Kiss <flowfast47@gmail.com>
2017-12-07 11:02:33 -0800

    add Hot Module Replacement

--- src/index.jsx
+++ src/index.jsx
@@ -2,118 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('react-app-root')
-);
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./components/App";
-//
-// class ProductCategoryRow extends React.Component {
-//   render() {
-//     const category = this.props.category;
-//     return (
-//       <tr>
-//         <th colSpan="2">
-//           {category}
-//         </th>
-//       </tr>
-//     );
-//   }
-// }
-//
-// class ProductRow extends React.Component {
-//   render() {
-//     const product = this.props.product;
-//     const name = product.stocked ?
-//       product.name :
-//       <span style={{color: 'red'}}>
-//         {product.name}
-//       </span>;
-//
-// return (
-//       <tr>
-//         <td>{name}</td>
-//         <td>{product.price}</td>
-//       </tr>
-//     );
-//   }
-// }
-//
-// class ProductTable extends React.Component {
-//   render() {
-//     const rows = [];
-//     let lastCategory = null;
-//     //line below is how the PRODUCTS got in
-//     this.props.products3.forEach((product) => {
-//       if (product.category !== lastCategory) {
-//         rows.push(
-//           <ProductCategoryRow
-//             category={product.category}
-//             key={product.category} />
-//         );
-//       }
-//       rows.push(
-//         <ProductRow
-//           product={product}
-//           key={product.name} />
-//       );
-//
-//       lastCategory = product.category;
-//     });
-//
-//     return (
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Name</th>
-//             <th>Price</th>
-//           </tr>
-//         </thead>
-//         <tbody>{rows}</tbody>
-//       </table>
-//     );
-//   }
-// }
-//
-// class SearchBar extends React.Component {
-//   render() {
-//     return (
-//       <form>
-//         <input type="text" placeholder="Search..." />
-//         <p>
-//           <input type="checkbox" />
-//           {' '}
-//           Only show products in stock
-//         </p>
-//       </form>
-//     );
-//   }
-// }
-//
-// class FilterableProductTable extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <SearchBar />
-//         <ProductTable products3={this.props.products2} />
-//       </div>
-//     );
-//   }
-// }
-//
-//
-// const PRODUCTS = [
-//   {category: 'Sporting Goods', price: '$59.99', stocked: true, name: 'Football'},
-//   {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-//   {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-//   {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-//   {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-//   {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-// ];
-//
+import { AppContainer } from 'react-hot-loader';
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  });
+}
+// worked part 2
 // ReactDOM.render(
-//   <FilterableProductTable products2 = {PRODUCTS} />,
+//   <App/>,
 //   document.getElementById('react-app-root')
 // );
